c6e0f700a65617cc314c07d11097df7134e508e8
santoshkumarmehra <santoshkumarmehra99@gmail.com>
2023-06-12 18:54:31 +0530

    TemplateView and RedirectView

---
+++ school/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView, RedirectView
+
+
+
+
+
+
+
+
+
+# class Home(TemplateView):
+#     template_name='school/home.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['name'] = 'Santosh kumar mehra'
+#         print(kwargs)
+#         return context
+
+
